133408e11efdbde975e071ad6766c64251e605e3
Nickie777 <imnickolay@gmail.com>
2026-02-28 10:49:52 +0300

    simple вариант. Версия 2. + кнопка записи и сама запись в файл generated_scenario.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
+import subprocess
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from playwright.sync_api import sync_playwright
+from pytest_playwright.pytest_playwright import output_path
+
+VIDEO_DIR = "videos"
+os.makedirs(VIDEO_DIR, exist_ok=True)
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -20,3 +26,38 @@ def run_test():
         browser.close()
 
     return {"result": title}
+
+@app.get("/record")
+def record_test():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(
+            record_video_dir=VIDEO_DIR
+        )
+        page = context.new_page()
+
+        page.goto("https://www.google.com/")
+        page.get_by_role("link", name="Get started").click()
+
+        context.close()
+        browser.close()
+
+    # найдём последний записанный файл
+    files = sorted(os.listdir(VIDEO_DIR))
+    last_video = files[-1]
+
+    return {"video_file": last_video}
+
+@app.get("/codegen")
+def start_codegen():
+    output_file = "generated_scenario.py"
+    subprocess.Popen(
+        [
+            "playwright",
+            "codegen",
+            "https://playwright.dev/",
+            "--output",
+            output_file
+        ]
+    )
+    return {"status": "Codegen started"}
